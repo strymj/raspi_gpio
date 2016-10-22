de27861fff980aba2478c5db7d2e77372439d551
strymj <yamaji.shoutarou@gmail.com>
2016-10-22 18:14:30 +0900

    fisdt_commit

---
+++ addsudo.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cd ~/catkin_ws/devel/lib/raspi_gpio/
+
+expect -c"
+set timeout 10
+spawn sudo chown root:root $1
+expect \"sudo\"
+send \"$2\n\"
+"
+
+expect -c"
+set timeout 10
+spawn sudo chmod a+rw $1
+expect \"sudo\"
+send \"$2\n\"
+"
+
+expect -c"
+set timeout 10
+spawn sudo chmod u+s $1
+expect \"sudo\"
+send \"$2\n\"
+"
+
+echo "finish"
+
+exit 0;
---
+++ src/LEDflash_gpio18.cpp
@@ -0,0 +1,29 @@
+#include <ros/ros.h>
+#include <iostream>
+#include "wiringPi.h"
+#define GPIO18 18
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "LEDflash_gpio18");
+	ros::NodeHandle node_;
+	ros::Rate looprate(10);
+
+	if(wiringPiSetupGpio() == -1) return -1;
+	else cout<<"wiringPi initialize done"<<endl;
+	pinMode(GPIO18, OUTPUT);
+
+	int loopcount = 0;
+	while(ros::ok()) {
+	
+		if(loopcount/10%2) digitalWrite(GPIO18,1);
+		else digitalWrite(GPIO18,0);
+	
+		loopcount++;
+		looprate.sleep();
+	}
+
+	digitalWrite(GPIO18,0);
+	return 0;
+}
