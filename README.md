a7f3afc1caf0ca1eb7b834170efe296de0229712
strymj <yamaji.shoutarou@gmail.com>
2016-10-22 18:34:51 +0900

    add_README

---
+++ README.md
@@ -0,0 +1,18 @@
+raspberry pi 3 model B 
+ubuntu MATE 16.04
+ros kinetic
+
+usage
+
+1. clone wiringPi
+$ git clone git://git.drogon.net/wiringPi
+$ cd wiringPi
+$ ./build
+
+2. write program and catkin_make
+
+3. add authority (to run gpio)
+$ cd my_catkin_ws/devel/lib/raspi_gpio
+$ sudo chown root:rot my_node
+$ sudo chmod a+rw my_node
+$ sudo chmod u+s my_node
--- addsudo.sh
+++ addsudo.sh
@@ -2,26 +2,29 @@
 
 cd ~/catkin_ws/devel/lib/raspi_gpio/
 
-expect -c"
+expect -c "
 set timeout 10
 spawn sudo chown root:root $1
 expect \"sudo\"
 send \"$2\n\"
 "
 
-expect -c"
-set timeout 10
-spawn sudo chmod a+rw $1
-expect \"sudo\"
-send \"$2\n\"
-"
+#expect -c "
+#set timeout 10
+#spawn sudo chmod a+rw $1
+#expect \"sudo\"
+#send \"$2\n\"
+#"
+#
+#expect -c "
+#set timeout 10
+#spawn sudo chmod u+s $1
+#expect \"sudo\"
+#send \"$2\n\"
+#"
 
-expect -c"
-set timeout 10
-spawn sudo chmod u+s $1
-expect \"sudo\"
-send \"$2\n\"
-"
+sudo chmod a+rw $1
+sudo chmod u+s $1
 
 echo "finished"
 
