0d04676259764d497cecd064dabd38c51dacd7b5
strymj <yamaji.shoutarou@gmail.com>
2016-11-02 11:58:52 +0900

    change

--- src/omni_servo.cpp
+++ src/omni_servo.cpp
@@ -4,17 +4,17 @@
 #include <cmath>
 #include "wiringPi.h"
 #include "softPwm.h"
-#define MOTOR1A 17
-#define MOTOR1B 18
-#define MOTOR2A 22
-#define MOTOR2B 23
-#define MOTOR3A 6
-#define MOTOR3B 12
-#define RANGE 512
+#define MOTOR1A 4
+#define MOTOR1B 3
+#define MOTOR2A 23
+#define MOTOR2B 24
+#define MOTOR3A 11
+#define MOTOR3B 9
+#define RANGE 216 
 using namespace std;
 
 double wrad[3] = {0, M_PI*2/3, M_PI*4/3};
-double movecmd[3];
+double movecmd[3] = {0,0,0};
 bool subscribed = false;
 bool LED = false;
 
@@ -23,6 +23,7 @@ void movecmdCallback(const std_msgs::Float32MultiArray &msg)
 	movecmd[0] = msg.data[0];
 	movecmd[1] = msg.data[1];
 	movecmd[2] = msg.data[2];
+	cout<<"subscribed  data : "<<msg.data[0]<<","<<msg.data[1]<<","<<msg.data[2]<<endl;
 	subscribed = true;
 	LED = !LED;
 }
@@ -31,14 +32,19 @@ void movecmdCallback(const std_msgs::Float32MultiArray &msg)
 void calc_motorout(double motorout[], double movecmd[])
 {
 	for(int i=0; i<3; i++) {
-		motorout[i] = movecmd[i]*cos(wrad[i]) + movecmd[i]*sin(wrad[i]) + movecmd[i];
-		motorout[i] = motorout[i]/3;
+		motorout[i] = movecmd[0]*cos(wrad[i]) + movecmd[1]*sin(wrad[i]) + movecmd[2];
+		//motorout[i] = motorout[i]/3;
+		cout<<"motorout["<<i<<"] = "<<motorout[i]<<endl;
 	}
 }
 
 
 void PWMwrite(double motorout[])
 {
+	motorout[0] = motorout[0] * RANGE;
+	motorout[1] = motorout[1] * RANGE;
+	motorout[2] = motorout[2] * RANGE;
+
 	if(motorout[0]>=0) {
 		softPwmWrite(MOTOR1A, motorout[0]);
 		softPwmWrite(MOTOR1B, 0);
@@ -56,11 +62,11 @@ void PWMwrite(double motorout[])
 	}
 
 	if(motorout[2]>=0) {
-		softPwmWrite(MOTOR1A, motorout[2]);
-		softPwmWrite(MOTOR1B, 0);
+		softPwmWrite(MOTOR3A, motorout[2]);
+		softPwmWrite(MOTOR3B, 0);
 	} else {
-		softPwmWrite(MOTOR1A, 0);
-		softPwmWrite(MOTOR1B, -motorout[2]);
+		softPwmWrite(MOTOR3A, 0);
+		softPwmWrite(MOTOR3B, -motorout[2]);
 	}
 }
 
@@ -69,7 +75,7 @@ int main(int argc, char** argv)
 {
 	ros::init(argc, argv, "sub_LED");
 	ros::NodeHandle node_;
-	ros::Subscriber sub = node_.subscribe("/movecmd", 1, movecmdCallback);
+	ros::Subscriber sub = node_.subscribe("/keyjoy/movecmd", 1, movecmdCallback);
 	ros::Rate looprate(10);
 
 	if(wiringPiSetupGpio() == -1) {
